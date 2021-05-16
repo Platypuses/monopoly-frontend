4ffbc69c61d3f28bb92f001ed3823ece84c423a1
Casper <133casper@gmail.com>
2021-05-16 18:28:07 +0300

    Add BACKEND_WEBSOCKET_BASE_URL env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 ENV_VARIABLES='window.env = { \
   "BACKEND_BASE_URL":"'"${BACKEND_BASE_URL}"'", \
+  "BACKEND_WEBSOCKET_BASE_URL":"'"${BACKEND_WEBSOCKET_BASE_URL}"'", \
 }'
 
 sed -i "s@// ENV_PLACEHOLDER@${ENV_VARIABLES}@" /usr/share/nginx/html/index.html
